0bc6df27da35ec8a8d7c66ca7b624f218e03ca2a
Aditya Shrivastav <shriadishri@gmail.com>
2020-10-11 17:24:46 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {
+            colors: {
+                pri: '#192744',
+                sec: '#00FFAD',
+                tert: '#8519F7'
+            },
+            fontFamily: {
+                head: [
+                    'Inter',
+                    'system-ui',
+                    '-apple-system',
+                    'BlinkMacSystemFont',
+                    '"Segoe UI"',
+                    'Roboto',
+                    '"Helvetica Neue"',
+                    'Arial',
+                    '"Noto Sans"',
+                    'sans-serif',
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                    '"Noto Color Emoji"'
+                ],
+                body: [
+                    'Nunito',
+                    'system-ui',
+                    '-apple-system',
+                    'BlinkMacSystemFont',
+                    '"Segoe UI"',
+                    'Roboto',
+                    '"Helvetica Neue"',
+                    'Arial',
+                    '"Noto Sans"',
+                    'sans-serif',
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                    '"Noto Color Emoji"'
+                ]
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+};
